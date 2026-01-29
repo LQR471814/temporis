d7f3e7b9619733e742b000845b98a355a1f492c7
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-01-29 08:27:35 -0800

    make vertical controllable by view-controller

--- src/components/panes/vertical.tsx
+++ src/components/panes/vertical.tsx
@@ -9,19 +9,21 @@ import {
 	week,
 	year,
 } from "~/lib/timescales";
-import { cn, now } from "~/lib/utils";
+import { cn } from "~/lib/utils";
+import { useCurrentTime } from "src/context/view";
 
 export function VerticalTimeframes(props: { class?: string }) {
+	const currentTime = useCurrentTime();
 	return (
 		<div class={cn("flex flex-col gap-1 p-1 overflow-y-auto", props.class)}>
-			<Timeframe timescale={decade} time={now()} />
-			<Timeframe timescale={fiveyear} time={now()} />
-			<Timeframe timescale={year} time={now()} />
-			<Timeframe timescale={quarter} time={now()} />
-			<Timeframe timescale={month} time={now()} />
-			<Timeframe timescale={week} time={now()} />
-			<Timeframe timescale={day} time={now()} />
-			<Timeframe timescale={daypart} time={now()} />
+			<Timeframe timescale={decade} time={currentTime()} />
+			<Timeframe timescale={fiveyear} time={currentTime()} />
+			<Timeframe timescale={year} time={currentTime()} />
+			<Timeframe timescale={quarter} time={currentTime()} />
+			<Timeframe timescale={month} time={currentTime()} />
+			<Timeframe timescale={week} time={currentTime()} />
+			<Timeframe timescale={day} time={currentTime()} />
+			<Timeframe timescale={daypart} time={currentTime()} />
 		</div>
 	);
 }
