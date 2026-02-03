e3500e0dece45ff6fe5076cf77c728969e2ca6bc
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-02-02 20:29:24 -0800

    add from-file script

---
+++ schema/from-file.ts
@@ -0,0 +1,56 @@
+import "temporal-polyfill/global";
+
+import { drizzle } from "drizzle-orm/postgres-js";
+import { sql, eq } from "drizzle-orm";
+import postgres from "postgres";
+import { readFile } from "node:fs/promises";
+import { taskTable } from "./schema";
+import type { Tables } from "src/lib/supabase/types.gen";
+
+const client = postgres(
+	process.env.DB_URL ??
+		"postgresql://postgres:postgres@127.0.0.1:54322/postgres",
+);
+const db = drizzle({ client: client });
+
+async function main() {
+	const rows = JSON.parse(await readFile("tasks.json", "utf8"));
+	await db
+		.delete(taskTable)
+		.where(eq(taskTable.comments, "=== replace-with-imported ==="));
+	await db
+		.insert(taskTable)
+		.values(
+			rows.map((r: Tables<"task">) => {
+				if (!r.id) {
+					r.id = crypto.randomUUID();
+				}
+				return r;
+			}),
+		)
+		.onConflictDoUpdate({
+			target: taskTable.id,
+			set: {
+				assigned_to: sql`excluded.assigned_to`,
+				comments: sql`excluded.comments`,
+				implementation: sql`excluded.implementation`,
+				name: sql`excluded.name`,
+				optimistic: sql`excluded.optimistic`,
+				expected: sql`excluded.expected`,
+				pessimistic: sql`excluded.pessimistic`,
+				parent_id: sql`excluded.parent_id`,
+				timeframe_start: sql`excluded.timeframe_start`,
+				timescale: sql`excluded.timescale`,
+			},
+		});
+}
+
+main()
+	.then(() => {
+		console.log("Successfully inserted rows.");
+		process.exit(0);
+	})
+	.catch((err) => {
+		console.error(err);
+		process.exit(1);
+	});
