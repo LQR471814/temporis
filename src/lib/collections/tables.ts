c7edf046f362e73f3a04bb4dec7322afbd1979df
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-02-05 14:01:35 -0800

    fix mismatched tanstack db versions

--- src/lib/collections/tables.ts
+++ src/lib/collections/tables.ts
@@ -1,9 +1,17 @@
+import { createCollection } from "@tanstack/solid-db";
 import { trailBaseCollectionOptions } from "@tanstack/trailbase-db-collection";
 import { executors } from "../query";
 import { publicExecutorRowSchema } from "../supabase";
+import { trailBaseClient } from "../trailbase";
 import { tableQueryCollection } from "./utils";
 
-export const tasksCollection = createCollection(trailBaseCollectionOptions({}));
+export const tasksCollection = createCollection(
+	trailBaseCollectionOptions({
+		id: "task",
+		recordApi: trailBaseClient.records("task"),
+		getKey: (item) => item.id,
+	}),
+);
 tasksCollection.createIndex((row) => row.parent_id);
 
 export const executorsCollection = tableQueryCollection(
