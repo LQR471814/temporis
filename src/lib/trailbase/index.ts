efafaa2cceb5bb7fc70f660716d1e9728c61936a
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-02-05 17:53:16 -0800

    hotfix

--- src/lib/trailbase/index.ts
+++ src/lib/trailbase/index.ts
@@ -4,6 +4,4 @@ export const trailBaseClient = initClient(
 	import.meta.env.VITE_TRAILBASE_URL ?? "http://localhost:4000",
 );
 
-export * as executor from "./executor.gen";
-export * as executor_occupied from "./executor_occupied.gen";
-export * as task from "./task.gen";
+export * from "./types.gen";
