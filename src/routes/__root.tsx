7f58b4f5ab2e2485a9873416eac08cb32df07f59
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-03-13 23:03:42 -0700

    remove root grid

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,13 +4,13 @@ import { Toaster } from "src/components/ui/toast";
 
 export const Route = createRootRoute({
 	component: () => (
-		<div class="grid grid-rows-[min-content,minmax(0,1fr)] h-full">
+		<>
 			<main class="h-full">
 				<Outlet />
 			</main>
 			<Toaster />
 			{/* TanStack Router Devtools not yet available for Solid */}
-		</div>
+		</>
 	),
 	errorComponent: (props) => {
 		createEffect(() => {
